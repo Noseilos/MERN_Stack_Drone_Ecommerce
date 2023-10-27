ac8228d8373c8afa6e6102e7d6f87d05f7c3a177
Noseilos <jonaseguiron@gmail.com>
2023-10-27 11:10:53 +0800

    custom styles added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,13 +3,17 @@ import React from 'react'
 // --- STYLE IMPORTS --- 
 import { Navbar, Nav, Container } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
+import logo from '../assets/new.png'
 
 const Header = () => {
   return (
     <header>
         <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
             <Container>
-                <Navbar.Brand href='/'>NTech</Navbar.Brand>
+                <Navbar.Brand href='/'>
+                    <img src={logo} alt="NTech" style={{ marginRight: '10px' }}/>
+                    NTech
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls='basic-navbar-nav'/>
                 <Navbar.Collapse>
                     <Nav className="ms-auto">

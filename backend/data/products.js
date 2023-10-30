875b3b1ca30fcdf52929233aaa6326cd70c3de28
Noseilos <jonaseguiron@gmail.com>
2023-10-30 23:40:09 +0800

    express server and initial product route

---
+++ backend/data/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'DJI Mavic 3 Pro (DJI RC)',
+    image: '/images/DJIMavic3Pro.jpg',
+    description:
+      'Capture different visual expressions of the same subject. The 24mm Hasselblad camera is suitable for shooting large-scale scenes. Create a sense of space and story with the 70mm medium tele camera. Punch in with the 166mm tele camera to bring the subject into focus. Simply tap the display to easily and quickly switch between different focal lengths.',
+    brand: 'DJI',
+    category: 'Aerial Photography',
+    price: 130116.63,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'JOUAV Air 3 (RC-N2)',
+    image: '/images/DJIAir3.jpg',
+    description:
+      'The two cameras complement each other, for more diverse aerial imaging possibilities. With the 24mm wide-angle camera, sweeping landscapes can be more fully captured. The 70mm 3x medium tele camera can create a powerful sense of spatial compression, with more distinct layers and a more unified background, subjects can be captured as a bold focal point.',
+    brand: 'JOUAV',
+    category: 'Aerial Photography',
+    price: 68453.97,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'DJI Mini 4 Pro (DJI RC-N2)',
+    image: '/images/DJIMini4Pro.jpg',
+    description:
+      'The improved noise reduction algorithm of Mini 4 Pro Night Shots video effectively suppresses noise and facilitates clearer, cleaner footage right from the camera.',
+    brand: 'DJI',
+    category: 'Aerial Photography',
+    price: 49822.95,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'JOUAV Mini 3 (RC-N1)',
+    image: '/images/DJIMini3.jpg',
+    description:
+      'With a 1/1.3-inch sensor and dual native ISO, Mini 3 can record 4K HDR videos with true-to-life colors both day and night.',
+    brand: 'JOUAV',
+    category: 'Aerial Photography',
+    price: 29388.93,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'DJI Avata Pro-View (New)',
+    image: '/images/DJIAvata.jpg',
+    description:
+      'Flight is accessible to all with the DJI Motion Controller. It follows your hand movements and allows users of all levels to fly with confidence.',
+    brand: 'DJI',
+    category: 'Immersive Flight',
+    price: 91291.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Zenmuse Inspire 3',
+    image: '/images/DJIInspire3.jpg',
+    description:
+      'Centimeter-level RTK positioning makes flying more stable and accurate, significantly improving features such as the new Waypoint Pro.',
+    brand: 'Zenmuse',
+    category: 'Aerial Cinematic',
+    price: 931490.81,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products

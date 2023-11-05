d2f0867b8b68640eeddc67bb20db329dd2dd58e3
Noseilos <jonaseguiron@gmail.com>
2023-11-06 02:00:29 +0800

    remove from cart

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -5,7 +5,7 @@ import { FaTrash } from 'react-icons/fa'
 
 import Message from '../components/Message'
 
-import { addToCart } from '../slices/cartSlice'
+import { addToCart, removeFromCart } from '../slices/cartSlice'
 
 const CartScreen = () => {
     const navigate = useNavigate();
@@ -16,6 +16,14 @@ const CartScreen = () => {
 
     const addToCartHandler = async (product, qty) => {
         dispatch(addToCart({...product, qty}));
+    }
+    
+    const removeFromCartHandler = async (id) => {
+        dispatch(removeFromCart(id));
+    }
+
+    const checkOutHandler = () => {
+        navigate('/login?redirect=shipping');
     }
 
   return (
@@ -54,7 +62,7 @@ const CartScreen = () => {
                                     </Form.Control>
                                 </Col>
                                 <Col md={2}>
-                                    <Button type='button' variant='light'>
+                                    <Button type='button' variant='light' onClick={() => removeFromCartHandler(item._id)}>
                                         <FaTrash />
                                     </Button>
                                 </Col>
@@ -72,7 +80,8 @@ const CartScreen = () => {
                         ₱ { cartItems.reduce ((acc, item) => acc + item.qty * item.price, 0).toLocaleString() }
                     </ListGroup.Item>
                     <ListGroup.Item>
-                        <Button type='button' className='btn-block' disabled={ cartItems.length === 0 }>
+                        <Button type='button' className='btn-block' disabled={ cartItems.length === 0 }
+                            onClick={ checkOutHandler }>
                             Checkout
                         </Button>
                     </ListGroup.Item>

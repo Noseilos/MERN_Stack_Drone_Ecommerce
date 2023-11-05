98caa0de6f009ce78d17889841ebc4696c42fe81
Noseilos <jonaseguiron@gmail.com>
2023-11-06 01:07:16 +0800

    add to cart handler

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,89 +1,147 @@
-import React from 'react'
+import React from "react";
 
-// --- DATA IMPORTS --- 
-import Rating from '../components/Rating'
+// --- DATA IMPORTS ---
+import Rating from "../components/Rating";
 
 // --- COMPONENT IMPORT ---
-import Loader from '../components/Loader'
-import Message from '../components/Message'
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 
 // --- PACKAGE IMPORTS ---
-import { useParams, Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import { useParams, Link, useNavigate } from "react-router-dom";
+import {
+  Form,
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Button,
+  Card,
+} from "react-bootstrap";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
 
-// --- SLICE IMPORTS --- 
-import { useGetProductDetailsQuery } from '../slices/productsApiSlice'
+// --- SLICE IMPORTS ---
+import { useGetProductDetailsQuery } from "../slices/productsApiSlice";
+import { addToCart } from "../slices/cartSlice";
 
 const ProductScreen = () => {
+  const { id: productId } = useParams();
+  const [qty, setQty] = useState(1);
 
-    const { id: productId } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useGetProductDetailsQuery(productId);
+
+  const addToCartHandler = () => {
+    dispatch(addToCart({ ...product, qty }));
+    navigate('/cart');
+  };
 
   return (
     <>
-        <Link className='btn btn-light my-3' to='/'>Go Back</Link>
-        { isLoading ? (
-            <Loader />
-        ) : error ? (
-            <Message variant='danger'>
-              { error?.data?.message || error.error }
-            </Message>
-        ) : (
-            <>
-                <Row>
-                    <Col md={5}>
-                        <Image src={ product.image } alt={ product.name } fluid/>
-                    </Col>
-                    <Col md={4}>
-                        <ListGroup variant='flush'>
-                            <ListGroup.Item>
-                                <h3> { product.name } </h3>
-                            </ListGroup.Item>
-                            <ListGroup.Item>
-                                <Rating value={ product.rating } text={ `${product.numReviews} reviews` }/>
-                            </ListGroup.Item>
-                            <ListGroup.Item>Price: ₱ { product.price.toLocaleString() }</ListGroup.Item>
-                            <ListGroup.Item>{ product.description }</ListGroup.Item>
-                        </ListGroup>
-                    </Col>
-                    <Col md={3}>
-                        <Card>
-                            <ListGroup variant='flush'>
-                                <ListGroup.Item>
-                                    <Row>
-                                        <Col>Price: </Col>
-                                        <Col>
-                                            <strong>₱ { product.price.toLocaleString() }</strong>
-                                        </Col>
-                                    </Row>
-                                </ListGroup.Item>
-                                <ListGroup.Item>
-                                    <Row>
-                                        <Col>Status: </Col>
-                                        <Col>
-                                            <strong>{ product.countInStock > 0 ? 'In Stock' : 'Out of Stock' }</strong>
-                                        </Col>
-                                    </Row>
-                                </ListGroup.Item>
-                                
-                                <ListGroup.Item>
-                                    <Button
-                                        className='btn-block'
-                                        type='button'
-                                        disabled= { product.countInStock === 0 }
-                                        >
-                                        Add to Cart
-                                    </Button>
-                                </ListGroup.Item>
-                            </ListGroup>
-                        </Card>
-                    </Col>
-                </Row>
-            </>
-        ) }
-    </>
-  )
-}
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      ) : (
+        <>
+          <Row>
+            <Col md={5}>
+              <Image src={product.image} alt={product.name} fluid />
+            </Col>
+            <Col md={4}>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <h3> {product.name} </h3>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                  />
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  Price: ₱ {product.price.toLocaleString()}
+                </ListGroup.Item>
+                <ListGroup.Item>{product.description}</ListGroup.Item>
+              </ListGroup>
+            </Col>
+            <Col md={3}>
+              <Card>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Price: </Col>
+                      <Col>
+                        <strong>₱ {product.price.toLocaleString()}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Status: </Col>
+                      <Col>
+                        <strong>
+                          {product.countInStock > 0
+                            ? "In Stock"
+                            : "Out of Stock"}
+                        </strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
 
-export default ProductScreen
+                  {product.countInStock > 0 && (
+                    <ListGroup.Item>
+                      <Row>
+                        <Col>Qty</Col>
+                        <Col>
+                          <Form.Control
+                            as="select"
+                            value={qty}
+                            onChange={(e) => setQty(Number(e.target.value))}
+                          >
+                            {[...Array(product.countInStock).keys()].map(
+                              (x) => (
+                                <option key={x + 1} value={x + 1}>
+                                  {x + 1}
+                                </option>
+                              )
+                            )}
+                          </Form.Control>
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  )}
+
+                  <ListGroup.Item>
+                    <Button
+                      className="btn-block"
+                      type="button"
+                      disabled={product.countInStock === 0}
+                      onClick={addToCartHandler}
+                    >
+                      Add to Cart
+                    </Button>
+                  </ListGroup.Item>
+                </ListGroup>
+              </Card>
+            </Col>
+          </Row>
+        </>
+      )}
+    </>
+  );
+};
+
+export default ProductScreen;

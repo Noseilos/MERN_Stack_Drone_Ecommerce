0b281deadc979cda437e6a6c182616a69f44a773
Noseilos <jonaseguiron@gmail.com>
2023-11-06 00:13:22 +0800

    get product details endpoint

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -6,81 +6,76 @@ import Rating from '../components/Rating'
 // --- PACKAGE IMPORTS ---
 import { useParams, Link } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+
+// --- SLICE IMPORTS --- 
+import { useGetProductDetailsQuery } from '../slices/productsApiSlice'
 
 const ProductScreen = () => {
 
-    const [ product, setProduct ] = useState({});
-
     const { id: productId } = useParams();
-    
-    useEffect(() => {
-        const fetchProduct = async () => {
-            const { data } = await axios.get(`/api/products/${productId}`);
-            setProduct(data);
-        }
 
-        fetchProduct();
-    }, [productId]);
-
-    const formattedPrice = product.price ? product.price.toLocaleString('en-US', {
-        style: 'currency',
-        currency: 'PHP',
-    }) : 'Price not available';
+    const { data: product, isLoading, error } = useGetProductDetailsQuery(productId);
 
   return (
     <>
         <Link className='btn btn-light my-3' to='/'>Go Back</Link>
-        <Row>
-            <Col md={5}>
-                <Image src={ product.image } alt={ product.name } fluid/>
-            </Col>
-            <Col md={4}>
-                <ListGroup variant='flush'>
-                    <ListGroup.Item>
-                        <h3> { product.name } </h3>
-                    </ListGroup.Item>
-                    <ListGroup.Item>
-                        <Rating value={ product.rating } text={ `${product.numReviews} reviews` }/>
-                    </ListGroup.Item>
-                    <ListGroup.Item>Price: ₱ { formattedPrice }</ListGroup.Item>
-                    <ListGroup.Item>{ product.description }</ListGroup.Item>
-                </ListGroup>
-            </Col>
-            <Col md={3}>
-                <Card>
-                    <ListGroup variant='flush'>
-                        <ListGroup.Item>
-                            <Row>
-                                <Col>Price: </Col>
-                                <Col>
-                                    <strong>₱ { formattedPrice }</strong>
-                                </Col>
-                            </Row>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Row>
-                                <Col>Status: </Col>
-                                <Col>
-                                    <strong>{ product.countInStock > 0 ? 'In Stock' : 'Out of Stock' }</strong>
-                                </Col>
-                            </Row>
-                        </ListGroup.Item>
-                        
-                        <ListGroup.Item>
-                            <Button
-                                className='btn-block'
-                                type='button'
-                                disabled= { product.countInStock === 0 }
-                                >
-                                Add to Cart
-                            </Button>
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Card>
-            </Col>
-        </Row>
+        { isLoading ? (
+            <h2>Loading...</h2>
+        ) : error ? (
+            <div>{ error?.data?.message || error.error }</div>
+        ) : (
+            <>
+                <Row>
+                    <Col md={5}>
+                        <Image src={ product.image } alt={ product.name } fluid/>
+                    </Col>
+                    <Col md={4}>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <h3> { product.name } </h3>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Rating value={ product.rating } text={ `${product.numReviews} reviews` }/>
+                            </ListGroup.Item>
+                            <ListGroup.Item>Price: ₱ { product.price.toLocaleString() }</ListGroup.Item>
+                            <ListGroup.Item>{ product.description }</ListGroup.Item>
+                        </ListGroup>
+                    </Col>
+                    <Col md={3}>
+                        <Card>
+                            <ListGroup variant='flush'>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Price: </Col>
+                                        <Col>
+                                            <strong>₱ { product.price.toLocaleString() }</strong>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Row>
+                                        <Col>Status: </Col>
+                                        <Col>
+                                            <strong>{ product.countInStock > 0 ? 'In Stock' : 'Out of Stock' }</strong>
+                                        </Col>
+                                    </Row>
+                                </ListGroup.Item>
+                                
+                                <ListGroup.Item>
+                                    <Button
+                                        className='btn-block'
+                                        type='button'
+                                        disabled= { product.countInStock === 0 }
+                                        >
+                                        Add to Cart
+                                    </Button>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Card>
+                    </Col>
+                </Row>
+            </>
+        ) }
     </>
   )
 }

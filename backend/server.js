4da6bcf5156357cc651f26e79467b9c61774950f
Noseilos <jonaseguiron@gmail.com>
2023-11-06 15:46:22 +0800

    user routes and controller

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
 
@@ -15,7 +16,8 @@ app.get('/', (req, res) => {
     res.send('API is running ...');
 });
 
-app.use('/api/products/', productRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);

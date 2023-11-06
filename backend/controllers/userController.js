01e0499c0efce0d7c8926f555236f8383f2512e3
Noseilos <jonaseguiron@gmail.com>
2023-11-06 18:49:45 +0800

    user email and password validation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,22 @@ import User from '../models/userModel.js'
 // @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => { 
-    res.send('Auth User')
+    
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(401);
+        throw new Error('Invalid Email or Password!')
+    }
 });
 
 // @desc    Register user
--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,9 @@ const port = process.env.PORT || 5000;
 connectDB(); // Connect to MongoDB
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
     res.send('API is running ...');
 });

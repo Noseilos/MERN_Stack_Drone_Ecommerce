fd2abbcb914495079d100c4861cab88549ff4749
Noseilos <jonaseguiron@gmail.com>
2023-11-08 00:04:02 +0800

    logout functionality

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,10 +4,15 @@ import React from 'react'
 import { Navbar, Nav, Container, Badge, NavDropdown } from 'react-bootstrap'
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import { LinkContainer } from 'react-router-bootstrap'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
 // --- FILE IMPORTS ---
 import logo from '../assets/new.png'
+
+// --- SLICE IMPORT --- 
+import { useLogoutMutation } from '../slices/usersApiSlice'
+import { logout } from '../slices/authSlice'
 
 
 
@@ -16,8 +21,19 @@ const Header = () => {
     const { cartItems } = useSelector((state) => state.cart);
     const { userInfo } = useSelector((state) => state.auth);
 
-    const logoutHandler = () => {
-        console.log('logout handler');
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [ logoutApiCall ] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate('/login');
+        } catch (err) {
+            console.log(err)
+        }
     }
 
     console.log(cartItems)

ef8df503d3ae179ddcecb19ed88122af0078fae5
Noseilos <jonaseguiron@gmail.com>
2023-11-08 16:04:22 +0800

    create order enpoint and order screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,6 +24,7 @@ import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 // --- COMPONENT IMPORTS
 import PrivateRoute from './components/PrivateRoute';
@@ -40,6 +41,7 @@ const router = createBrowserRouter(
       <Route path='' element={ <PrivateRoute /> }>
         <Route path='/shipping' element={ <ShippingScreen /> }/>
         <Route path='/payment' element={ <PaymentScreen /> }/>
+        <Route path='/placeorder' element={ <PlaceOrderScreen /> }></Route>
       </Route>
     </Route>
   )

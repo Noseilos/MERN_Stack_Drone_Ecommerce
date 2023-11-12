67fc88342de2f417bc5ce74eb1a7b705cab7ebb2
Noseilos <jonaseguiron@gmail.com>
2023-11-13 00:12:15 +0800

    orderlist page, admin dropdown

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -126,7 +126,7 @@ const ProfileScreen = () => {
                     { error?.data?.message || error.error }
                 </Message>
             ) : (
-                <Table striped border hover responsive className="table-sm">
+                <Table striped hover responsive className="table-sm">
                     <thead>
                         <tr>
                             <th>ID</th>
@@ -142,7 +142,7 @@ const ProfileScreen = () => {
                             <tr key={order._id}>
                                 <td>{order._id}</td>
                                 <td>{order.createdAt.substring(0, 10)}</td>
-                                <td>{order.totalPrice}</td>
+                                <td>₱{ parseFloat(order.totalPrice).toLocaleString() }</td>
                                 <td>
                                     { order.isPaid ? (
                                         order.paidAt.substring(0, 10)
